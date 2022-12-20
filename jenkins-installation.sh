932ee448114e5f75ec026ead49d64c57217e7abc
siddhesh724 <siddheshtendulkar724@gmail.com>
2022-12-20 17:06:59 +0530

    jenkins install done

---
+++ jenkins-installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ubuntu 20.04 select and t2.small or medium instance has to select for good performance
+sudo apt install openjdk-11-jdk -y
+sudo apt update -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
